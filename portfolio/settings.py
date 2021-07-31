86e00f41c4a0d1007649ca41420ec00098bcb691
sohel284-coder <srana.bscse151284@gmail.com>
2021-07-31 17:57:50 +0600

    changes static root

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -145,10 +145,8 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 django_heroku.settings(locals())
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
